879bd4ff32b87f717ec1def5c708da775e91de88
Lizzy <ea.giovanniello@gmail.com>
2022-02-22 17:19:17 -0500

    updated views to include permission classes.

--- backend_apps/social_backend/views.py
+++ backend_apps/social_backend/views.py
@@ -2,22 +2,29 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from django.contrib.auth.decorators import login_required
 from rest_framework.decorators import api_view, permission_classes
 from .models import Comment, Reply, Like, Dislike
 from .serializers import CommentSerializer, ReplySerializer, LikeSerializer, DislikeSerializer
 from django.contrib.auth.models import User
 
-@api_view(['GET', 'POST'])
-def CommentsList(request):
-  match request.method:
-    case 'GET':
-      comments = Comment.objects.all()
-      serializer = CommentSerializer(comments, many=True)
-      return Response(serializer.data)
-    case 'POST':
-      serializer = CommentSerializer(data=request.data)
-      if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-      return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def get_all_comments(request):
+  comments = Comment.objects.all()
+  serializer = CommentSerializer(comments, many=True)
+  return Response(serializer.data)
+
+
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def add_comment(request):
+  serializer = CommentSerializer(data=request.data)
+  if serializer.is_valid():
+    serializer.save(user=request.user)
+    return Response(serializer.data, status=status.HTTP_201_CREATED)
+  return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
