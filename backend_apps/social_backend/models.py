67740c4665a69e29816d2285437107ac6fed47b9
dstoffels <dan.stoffels@gmail.com>
2022-02-22 14:42:11 -0600

    added serializers, CommentsList view

--- backend_apps/social_backend/models.py
+++ backend_apps/social_backend/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Comment(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     video_id = models.CharField(max_length=500)
     body = models.CharField(max_length=500)
 
@@ -18,7 +18,6 @@ class Reply(Response):
 
 class ReplyID(models.Model):
     reply_id = models.ForeignKey(Reply, on_delete=models.CASCADE, blank=True, null=True)
-
     class Meta:
         abstract = True
 
---
+++ backend_apps/social_backend/serializers.py
@@ -0,0 +1,34 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from .models import Comment, Reply, Like, Dislike
+
+class UserSerializer(serializers.ModelSerializer):
+  class Meta:
+     model = User
+     fields = ['id', 'username']
+
+class LikeSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Like
+    fields = ['id', 'user_id', 'reply_id', 'comment_id']
+    
+class DislikeSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Like
+    fields = ['id', 'user_id', 'reply_id', 'comment_id']
+
+class CommentSerializer(serializers.ModelSerializer):
+  user = UserSerializer(many=False, read_only=True)
+  likes = LikeSerializer(many=True)
+  dislikes = DislikeSerializer(many=True)
+  class Meta:
+    model = Comment
+    fields=  ['id', 'user_id', 'body', 'video_id']
+
+class ReplySerializer(serializers.ModelSerializer):
+  user = UserSerializer(many=False, read_only=True)
+  likes = LikeSerializer(many=True)
+  dislikes = DislikeSerializer(many=True)
+  class Meta:
+    model = Reply
+    fields = ['id','body', 'user_id', 'comment_id']
